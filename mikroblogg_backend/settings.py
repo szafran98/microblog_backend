ed4dc24b2ab6fa56bb2b6ee596da07bdbc9c9d8f
szafran98 <szafran1308@gmail.com>
2020-08-14 15:36:07 +0200

    Added custom user model and initialized user manager tests

--- mikroblogg_backend/settings.py
+++ mikroblogg_backend/settings.py
@@ -109,6 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'user.CustomUser'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
--- user/models.py
+++ user/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
 
 # Create your models here.
+
+class CustomUser(AbstractUser):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
