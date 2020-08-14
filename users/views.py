ed9e09439f511e96832c0cd3e7c2da423649a11e
szafran98 <szafran1308@gmail.com>
2020-08-14 16:36:27 +0200

    Added user views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import CustomUser
+from .serializers import UserSerializer
 
 # Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = CustomUser.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
