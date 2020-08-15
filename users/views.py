156ab5ff61aa40830a2c77bbc34252be43ea65c6
szafran98 <szafran1308@gmail.com>
2020-08-15 14:24:07 +0200

    Custom auth token service

--- users/views.py
+++ users/views.py
@@ -1,4 +1,8 @@
 from rest_framework import viewsets
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.exceptions import AuthenticationFailed
+from rest_framework.response import Response
 from rest_framework.permissions import IsAdminUser
 from .models import CustomUser
 from .serializers import UserSerializer
@@ -10,3 +14,18 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
     #permission_classes = [IsAdminUser]
+
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        try:
+            serializer = self.serializer_class(data=request.data, context={'request': request})
+            serializer.is_valid(raise_exception=True)
+            user = serializer.validated_data['user']
+            token, created = Token.objects.get_or_create(user=user)
+            return Response({
+                'token': token.key
+            })
+        except AttributeError:
+            raise AuthenticationFailed()
