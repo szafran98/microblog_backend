04a696cf73318964c011fa6674ebed81095aefdd
szafran98 <szafran1308@gmail.com>
2020-08-16 14:23:57 +0200

    Modified microblog urls

--- mikroblogg_backend/urls.py
+++ mikroblogg_backend/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('users.urls')),
+    path('api/', include('users.urls', 'microblog.urls')),
     path('api-auth/', include('rest_framework.urls')),
 ]
