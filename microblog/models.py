793ddb491f0b0f35dcd2e478360a21c02d8eebec
szafran98 <szafran1308@gmail.com>
2020-08-16 13:06:41 +0200

    Added post and comment models

--- microblog/models.py
+++ microblog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+
 
 # Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    date_pub = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(to=get_user_model())
+    tags = models.CharField(max_length=100)
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    content = models.TextField(max_length=300)
+    to_post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    date_pub = models.DateTimeField(auto_now_add=True)
