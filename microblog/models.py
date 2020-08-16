7972825477a152bbf87276200e57b601d71b4c91
szafran98 <szafran1308@gmail.com>
2020-08-16 15:56:04 +0200

    Related name fixed

--- microblog/models.py
+++ microblog/models.py
@@ -43,7 +43,7 @@ class Comment(models.Model):
     content = models.TextField(max_length=300)
     to_post = models.ForeignKey(Post, on_delete=models.CASCADE)
     date_pub = models.DateTimeField(auto_now_add=True)
-    liked = models.ManyToManyField(to=get_user_model(), related_name='PostLikeToggle')
+    liked = models.ManyToManyField(to=get_user_model(), related_name='CommentLikeToggle')
 
     def date_pub_timestamp(self):
         return self.date_pub.timestamp()
