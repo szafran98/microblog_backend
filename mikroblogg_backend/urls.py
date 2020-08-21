df44f3d8b2e1e23dec0f4aab0d95ea8ccdb7af20
szafran98 <szafran1308@gmail.com>
2020-08-21 22:13:29 +0200

    Added djoser.urls to path auth in project urls

--- mikroblogg_backend/urls.py
+++ mikroblogg_backend/urls.py
@@ -15,12 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users.views import CustomAuthToken
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('api/', include('microblog.urls')),
     path('api-auth/', include('rest_framework.urls')),
-    path('auth/', CustomAuthToken.as_view()),
+    path('auth/', include('djoser.urls')),
 ]
