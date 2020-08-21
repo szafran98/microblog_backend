681f52599dc6c06231b101a3ce0e713d4b709281
szafran98 <szafran1308@gmail.com>
2020-08-21 23:29:16 +0200

    Deleted users router and urls, djoser will handle it

--- mikroblogg_backend/urls.py
+++ mikroblogg_backend/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/', include('users.urls')),
+    #path('users/', include('users.urls')),
     path('api/', include('microblog.urls')),
     path('api-auth/', include('rest_framework.urls')),
     path('auth/', include('djoser.urls')),
--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,3 @@
 from rest_framework.routers import DefaultRouter
 from django.urls import include, path
 from .views import UserViewSet, CustomAuthToken
-
-
-router = DefaultRouter()
-router.register('', UserViewSet)
-
-
-urlpatterns = [
-    path('', include(router.urls)),
-    path('auth/', CustomAuthToken.as_view()),
-]
