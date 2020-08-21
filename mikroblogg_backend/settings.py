8c7c0b23a111134c8eaaba3bb94b2b0e4caaa115
szafran98 <szafran1308@gmail.com>
2020-08-21 23:27:09 +0200

    Added djoser configuration in settings

--- mikroblogg_backend/settings.py
+++ mikroblogg_backend/settings.py
@@ -142,3 +142,13 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
    'AUTH_HEADER_TYPES': ('JWT',),
 }
+
+DJOSER = {
+    'PASSWORD_RESET_CONFIRM_URL': '#/password/reset/confirm/{uid}/{token}',
+    'USERNAME_RESET_CONFIRM_URL': '#/username/reset/confirm/{uid}/{token}',
+    'ACTIVATION_URL': '#/activate/{uid}/{token}',
+    'SEND_ACTIVATION_EMAIL': False,
+    'LOGIN_FIELD': 'email', # DO SPRAWDZENIE Z CUSTOM USEREM
+    'TOKEN_MODEL': None,
+
+}
